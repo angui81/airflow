8dcbfa9826e2488b5e4e2a471c3fe94e23e758d2
lucidcoder <lucidcoder81@gmail.com>
2025-12-08 10:29:23 +0900

    group 다시 정의

--- dags/dags_python_with_task_group.py
+++ dags/dags_python_with_task_group.py
@@ -14,12 +14,12 @@ with DAG(dag_id="dags_python_with_task_group", schedule="30 6 * * *", start_date
         '''task_group 데커레이터를 이용한 첫 번째 그룹입니다.'''
 
     @task(task_id='inner_function1')
-    def inner_func1(**kwargs):
+    def inner_function1(**kwargs):
         print('첫 번째 TaskGroup 내 첫 번째 task 입니다.')
 
-    inner_function2 = PythonOperator(task_id='inner_func2', python_callable=inner_func, op_kwargs={'msg': '첫 번째 TaskGroup내 두 번째 task 입니다'})
+    inner_function2 = PythonOperator(task_id='inner_function2', python_callable=inner_func, op_kwargs={'msg': '첫 번째 TaskGroup내 두 번째 task 입니다'})
 
-    inner_func1() >> inner_function2
+    inner_function1() >> inner_function2
 
     with TaskGroup(group_id='second_group', tooltip='두 번째 그룹입니다') as group_2:
         '''여기에 적은 docstring은 표시되지 않습니다'''
@@ -29,3 +29,5 @@ with DAG(dag_id="dags_python_with_task_group", schedule="30 6 * * *", start_date
             print('두 번쨰 TaskGroup 내 첫 번쨰 task 입니다.')
 
         inner_function2 = PythonOperator(task_id='inner_function2', python_callable=inner_func, op_kwargs={'msg': '두번쨰 TaskGroup 내 두번째 Task 입니다.'})
+
+    group_1() >> group_2
