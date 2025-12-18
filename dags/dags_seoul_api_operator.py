655db193b2c0ad454e8a05ea14d11ba424d1b2ca
lucidcoder <lucidcoder81@gmail.com>
2025-12-18 16:18:46 +0900

    timezone 보완

--- dags/dags_seoul_api_operator.py
+++ dags/dags_seoul_api_operator.py
@@ -7,7 +7,7 @@ with DAG(dag_id="dags_seoul_api", schedule="30 6 * * *", start_date=pendulum.dat
     tb_corona19_count_status = SeoulApiToCsvOperator(
         task_id='tb_corona19_count_status',
         dataset_nm='TbCorona19CountStatus',
-        path='/opt/airflow/files/TbCorona19CountStatus/{{data_interval_end.in_timezone("Asia/seoul") | ds_nodash}}',
+        path='/opt/airflow/files/TbCorona19CountStatus/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash}}',
         file_name='TbCorona19CountStatus.csv',
     )
 
@@ -15,7 +15,7 @@ with DAG(dag_id="dags_seoul_api", schedule="30 6 * * *", start_date=pendulum.dat
     tv_corona19_vaccine_stat_new = SeoulApiToCsvOperator(
         task_id='tv_corona19_vaccine_stat_new',
         dataset_nm='TvCorona19VaccinestatNew',
-        path='/opt/airflow/files/TvCorona19VaccinestatNew/{{data_interval_end.in_timezone("Asia/seoul") | ds_nodash}}',
+        path='/opt/airflow/files/TvCorona19VaccinestatNew/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash}}',
         file_name='TvCorona19VaccinestatNew.csv',
     )
 
